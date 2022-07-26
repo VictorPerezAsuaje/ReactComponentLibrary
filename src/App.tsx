1a469493790d80e52711b66cb20d9403b9b1eb98
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2022-07-26 12:41:05 +0200

    test gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from './components/Atoms/Button/Button'
 
 export default function App() {
   return (
     <div className="App">
       Hi there!
+      <Button />
     </div>
   );
 }
